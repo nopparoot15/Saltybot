7b0c1d19cc697cb8870940ad5e21bb12a8259e78
nopparoot15 <99222124+nopparoot15@users.noreply.github.com>
2025-10-19 00:57:18 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ intents.message_content = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents, help_command=None)
 
 @bot.event
 async def on_ready():
